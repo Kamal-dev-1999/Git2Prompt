66d22bac9eb0548dc57ee6bb3b1f102f0d56b8b2
kamal-dev-1999 <kamaltripathi1431@gmail.com>
2026-05-13 23:19:41 +0530

    Enabled Vercel Speed insights

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Mono, Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const spaceMono = Space_Mono({
